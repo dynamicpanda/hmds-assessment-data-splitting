3de2e08f10d6b2c624d6d4064beb1f36d368a9fa
Nathan Tasker <nmtask@gmail.com>
2022-01-03 22:17:46 -0500

    Added address merging and grouping

--- python/data_splitter.py
+++ python/data_splitter.py
@@ -21,6 +21,7 @@ import csv
 import logging
 import json
 import os
+import pprint
 import socket
 import sys
 
@@ -30,7 +31,9 @@ from collections import defaultdict
 logging.getLogger().setLevel(logging.DEBUG)
 
 # define constants
+ADDRESS_FIELDS = ('STREET', 'CITY', 'ZIP', 'COUNTRY')
 DELIMITER = ','
+GROUP_BY_FIELDS = ('GROUP', 'COUNTRY')
 JSON_EXT = '.json'
 FINAL_JSON_NAME = 'final' + JSON_EXT
 OUTPUT_LOCATION = os.path.join(
@@ -54,8 +57,8 @@ class Record(dict):
         """
         super().__init__(*args, **kwargs)
 
-        self.__sequence_id = sequence_id
-        self.sub_ids = defaultdict(set)
+        self['SEQUENCE_ID'] = sequence_id
+        self['MERGED_SEQUENCE_IDS'] = []
 
     @property
     def sequence_id(self):
@@ -64,7 +67,16 @@ class Record(dict):
         :return: The sequence ID of the record
         :rtype: str
         """
-        return self.__sequence_id
+        return self['SEQUENCE_ID']
+
+    @property
+    def merged_sequence_ids(self):
+        """Get the merged sequence IDs within this record
+
+        :return: The merged sequenced IDs within the record
+        :rtype: list
+        """
+        return self['MERGED_SEQUENCE_IDS']
 
     def merge(self, other_record):
         """Merge the record provided into this record. Prioritize the data of
@@ -76,25 +88,40 @@ class Record(dict):
         :param other_record: The record to merge into this one.
         :type other_record: Record
         """
-        self.sub_ids.add(other_record.sequence_id)
+        self['MERGED_SEQUENCE_IDS'].append(other_record.sequence_id)
 
 
-class RecordList(list):
-    def merge(self, main_record_index, *merge_indices):
-        """Merge the records in the positions provided, preserving the first
-        record and removing all others.
+class Batch(dict):
 
-        :param main_record_index: The index of the main record into which the
+    def add(self, record):
+        """Add the record provided to the batch, using its sequence ID as the
+        dictionary key.
+
+        :param record: The record to add to the batch
+        :type record: Record
+        """
+
+        if not isinstance(record, Record):
+            raise TypeError(f'Invalid type: {type(record)}. Records must be '
+                            'of \'Record\' type')
+
+        self[record.sequence_id] = record
+
+    def merge(self, main_record_id, *merge_ids):
+        """Merge the records with the IDs provided into the main record with
+        the main record ID provided, removing all merged records.
+
+        :param main_record_id: The ID of the main record into which the
         other(s) will be merged.
-        :type main_record_index: int
-        :param merge_indices: The indices in the list of the records which will
-        be merged into the main record.
-        :type merge_indices: tuple
+        :type main_record_id: str
+        :param merge_ids: The IDs in the batch of the records which will be
+        merged into the main record.
+        :type merge_ids: tuple
         """
 
         # merge and remove all provided records
-        for index in merge_indices:
-            self[main_record_index].merge(self.pop(index))
+        for merge_id in merge_ids:
+            self[main_record_id].merge(self.pop(merge_id))
 
 
 if __name__ == '__main__':
@@ -104,25 +131,62 @@ if __name__ == '__main__':
         csv_path = sys.argv[1]
         logging.info(f'Processing file {csv_path}...')
 
+        batch = Batch()
+
         if not os.path.isdir(OUTPUT_LOCATION):
             os.makedirs(OUTPUT_LOCATION)
 
         # read the records from the CSV file
         with open(csv_path, newline='', encoding='utf-8-sig') as csv_file:
             reader = csv.DictReader(csv_file, delimiter=DELIMITER)
-            all_records = {}
 
             # generate a record object for each row in the CSV file
             for row in reader:
-                logging.debug(f'Row: {row}')
-                record = Record(row['SEQUENCE_ID'], **row)
-                logging.debug(f'Record: {record}')
-                all_records[record.sequence_id] = record
+                batch.add(Record(row['SEQUENCE_ID'], **row))
+        logging.debug(f'Batch: {pprint.pformat(batch)}')
+
+        # merge records with same address info
+        addresses = defaultdict(set)
+        for sequence_id in list(batch.keys()):
+            # combine address fields as one string to act as a key
+            # TODO: Clarify whether whitespace stripping is preferred
+            record = batch[sequence_id]
+            address = ''.join((record[field] for field in ADDRESS_FIELDS))
+            addresses[address].add(sequence_id)
+
+        # merge sequence IDs with like addresses into the record with the
+        # lowest sequence ID
+        for address, sequence_ids in addresses.items():
+            if len(sequence_ids) > 1:
+                sorted_sequence_ids = sorted(sequence_ids)
+                main_record_id = sorted_sequence_ids[0]
+                merge_record_ids = sorted_sequence_ids[1:]
+
+                logging.info(f'Merging IDs {main_record_id} into record '
+                             f'{merge_record_ids}...')
+                batch.merge(main_record_id, *merge_record_ids)
 
         # dump the records into the final JSON file
         final_json_path = os.path.join(OUTPUT_LOCATION, FINAL_JSON_NAME)
+        logging.info(f'Writing all records to {final_json_path}...')
         with open(final_json_path, 'w') as final_json_file:
-            json.dump(all_records, final_json_file, indent=4)
+            json.dump(batch, final_json_file, indent=4, sort_keys=True)
+
+        # group batch by defined fields
+        groups = defaultdict(Batch)
+        for sequence_id, record in batch.items():
+            key = tuple(record[field] for field in GROUP_BY_FIELDS)
+            groups[key].add(record)
+        logging.debug(f'Groups: {pprint.pformat(groups)}')
+
+        # dump each group of records into its own JSON file
+        for group, batch in groups.items():
+            group_json_path = os.path.join(OUTPUT_LOCATION,
+                                           '_'.join(group) + JSON_EXT)
+            logging.info(
+                f'Writing group {group} records to {group_json_path}...')
+            with open(group_json_path, 'w') as group_json_file:
+                json.dump(batch, group_json_file, indent=4, sort_keys=True)
 
     except Exception:
         logging.exception(f'Exception occurred while processing {csv_path}')
